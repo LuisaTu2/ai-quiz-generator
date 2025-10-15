eecb701b64b277680d1800b88cdf8d22ffb99522
LuisaTu2 <luisa.pegoraro@gmail.com>
2025-10-14 20:30:45 -0400

    [isa] http --> https

--- frontend/src/components/utils/constants.ts
+++ frontend/src/components/utils/constants.ts
@@ -1,46 +1,60 @@
 /* types */
-export type Language = "english" | "french" | "italian" | "spanish" | "serbian"
-export type Level = "beginner" | "intermediate" | "advanced"
-export type Topic = "food" | "fashion" | "travel" | "space"
- | "geography" | "sports" | "nature" 
-
+export type Language = "english" | "french" | "italian" | "spanish" | "serbian";
+export type Level = "beginner" | "intermediate" | "advanced";
+export type Topic =
+  | "food"
+  | "fashion"
+  | "travel"
+  | "space"
+  | "geography"
+  | "sports"
+  | "nature";
 
 export interface LearningOptions {
-    language?: Language
-    level?: Level
-    topics?: Topic[]
+  language?: Language;
+  level?: Level;
+  topics?: Topic[];
 }
-
 
 export interface QuestionType {
-    question: string
-    options: string[]
-    answer: string
-}
- 
-export interface Quiz {
-    questions: QuestionType[]
+  question: string;
+  options: string[];
+  answer: string;
 }
 
+export interface Quiz {
+  questions: QuestionType[];
+}
 
 /* constants */
-export const languages: Language[] =  ["english", "french", "italian", "serbian", "spanish"] 
-export const levels: Level[] = ["beginner", "intermediate", "advanced"]
-export const topicOptions: Topic[] = ["fashion", "food", "geography", "nature", "space", "sports", "travel"]
+export const languages: Language[] = [
+  "english",
+  "french",
+  "italian",
+  "serbian",
+  "spanish",
+];
+export const levels: Level[] = ["beginner", "intermediate", "advanced"];
+export const topicOptions: Topic[] = [
+  "fashion",
+  "food",
+  "geography",
+  "nature",
+  "space",
+  "sports",
+  "travel",
+];
 
-export const HTTP_PATH: string = "http://18.216.159.247/learn-api/"
+export const HTTPS_PATH: string = "https://learn.findingLuisa.com/learn-api/";
 
 export const firstUpperCase = (s: string) => {
-    if (s === ""){
-        return s
-    }
-    const firstLetter = s[0];
-    return firstLetter.toUpperCase() + s.slice(1) 
-}
+  if (s === "") {
+    return s;
+  }
+  const firstLetter = s[0];
+  return firstLetter.toUpperCase() + s.slice(1);
+};
 
-export const EMAILJS_SERVICE_ID = "service_strapazza_palle"
-export const EMAILJS_TEMPLATE_ID = "template_r9luivw"
-export const EMAILJS_PUBLIC_KEY = "5X2vLQdXV_I2c1gXZ"
-
-
-
+export const EMAILJS_SERVICE_ID = "service_strapazza_palle";
+export const EMAILJS_TEMPLATE_ID = "template_r9luivw";
+export const EMAILJS_PUBLIC_KEY = "5X2vLQdXV_I2c1gXZ";
--- frontend/src/components/utils/getQuizData.ts
+++ frontend/src/components/utils/getQuizData.ts
@@ -1,38 +1,48 @@
-import { HTTP_PATH, type Quiz, type Topic } from "./constants";
+import { HTTPS_PATH, type Quiz, type Topic } from "./constants";
 
 interface GetQuizDataProps {
-    language: string,
-    level: string,
-    topics: Topic[],
-    setIsLoading: (value: React.SetStateAction<boolean>) => void,
-    setQuiz: (value: React.SetStateAction<Quiz>) => void
-    setSlide: (value: React.SetStateAction<number>) => void
-    setError: React.Dispatch<React.SetStateAction<boolean>>
+  language: string;
+  level: string;
+  topics: Topic[];
+  setIsLoading: (value: React.SetStateAction<boolean>) => void;
+  setQuiz: (value: React.SetStateAction<Quiz>) => void;
+  setSlide: (value: React.SetStateAction<number>) => void;
+  setError: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const getQuizData = async ({language, level, topics, setIsLoading, setQuiz, setSlide, setError}: GetQuizDataProps) => {
-    setIsLoading(true)
-    setQuiz({questions: []})
+const getQuizData = async ({
+  language,
+  level,
+  topics,
+  setIsLoading,
+  setQuiz,
+  setSlide,
+  setError,
+}: GetQuizDataProps) => {
+  setIsLoading(true);
+  setQuiz({ questions: [] });
 
-    for (let i = 0; i < 3; i++){
-        try {
-            const response = await fetch(`${HTTP_PATH}?language=${language}&level=${level}&topic=${topics}`);
-            const result = await response.json();
-            
-            if (result){
-                setQuiz(prev => ({ ...prev, ...result }));
-                setIsLoading(false);
-                setSlide(5);
-                break
-            }
-        } catch (error) {
-            console.error(error);
-            if(i !== 0){
-                setSlide(0)
-                setError(true)
-            }
-        }
+  for (let i = 0; i < 3; i++) {
+    try {
+      const response = await fetch(
+        `${HTTPS_PATH}?language=${language}&level=${level}&topic=${topics}`
+      );
+      const result = await response.json();
+
+      if (result) {
+        setQuiz((prev) => ({ ...prev, ...result }));
+        setIsLoading(false);
+        setSlide(5);
+        break;
+      }
+    } catch (error) {
+      console.error(error);
+      if (i !== 0) {
+        setSlide(0);
+        setError(true);
+      }
     }
-}
+  }
+};
 
 export default getQuizData;
